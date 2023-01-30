2ebb26536b74ce7c8e234c751533f45be92873b1
CebotaruDaniel <daniel.cebotaru@iis.utm.md>
2023-01-30 21:18:38 +0200

    f

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',
+    './src/**/*.html',
+    './src/**/*.js',
+    './public/**/*.html',
+    './public/**/*.js'
   ],
-  // ...
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
